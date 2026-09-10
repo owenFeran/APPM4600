c34d9a6dd341b0d0491dd46e8e1e7b294d3f0a13
owenFeran <owen.feran@colorado.edu>
2026-09-10 17:52:00 -0600

    finished HW2

--- Homeworks/HW2/homework2.py
+++ Homeworks/HW2/homework2.py
@@ -22,3 +22,39 @@ f=x-4*np.sin(2*x)-3
 plt.plot(x,f)
 plt.plot(x,0*x)
 plt.show()
+
+f2= lambda x: (1/6)*x**2
+x3=9.999999995000000*(10**-10)
+f2(x3)
+
+#4
+f3= lambda x:2*x-1-np.sin(x)
+x4=np.linspace(-10,10)
+plt.plot(x4,f3(x4))
+plt.show()
+
+#6
+f4= lambda x: 4*np.sin(2*x)+3
+x4=np.linspace(-1,8,100)
+plt.plot(x4,f4(x4))
+plt.plot(x4,x4)
+plt.show()
+
+f5= lambda x: -np.sin(2*x)+5*x/4-3/4
+accFunc= lambda x:abs(-2*np.cos(2*x)+5/4 -1)*10**(-10)
+
+
+def fixedPtHw(f,Nmax,x0,accFunc):
+    count=0
+    while abs(x1-x0) > accFunc(x1):
+        x1=f(x0)
+        while count<=Nmax:
+            x0=x1
+            x1=f(x0)
+            count+=1
+        return (x1,count)
+    print('Error, reached max number of iterations')
+    return
+    
+res=fixedPtHw(f5,100,3.1,accFunc)
+print(res)
